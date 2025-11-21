2c4557339bc22d1ab1fed6d433ec6d72a968f33d
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-21 22:32:13 +0530

    Initial Commit on Design Pattern 2

--- src/C1_Strategy_Pattern/AdventureGame/temp.java
+++ src/C1_Strategy_Pattern/AdventureGame/temp.java
@@ -3,5 +3,6 @@ package C1_Strategy_Pattern.AdventureGame;
 public class temp {
     public static void main(String[] args) {
         System.out.println("Trial");
+        System.out.println("test");
     }
 }
