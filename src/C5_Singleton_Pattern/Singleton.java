fe6dcdeca622a740d8d0bdae15bee7a6a9096f2d
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-11 13:37:57 +0530

    Committing Chapter 5 Singleton pattern Completed

---
+++ src/C5_Singleton_Pattern/Singleton.java
@@ -0,0 +1,14 @@
+package C5_Singleton_Pattern;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+    // other useful instance variables here
+    private Singleton() {}
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+    // other useful methods here
+}
